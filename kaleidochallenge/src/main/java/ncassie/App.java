cc1b86208cea9e46a986cffe3c161dc319a9ba25
ncassie <57377583+ncassie@users.noreply.github.com>
2020-03-26 18:31:37 -0400

    Update App.java
    Added print statements to show program control

--- kaleidochallenge/src/main/java/ncassie/App.java
+++ kaleidochallenge/src/main/java/ncassie/App.java
@@ -17,12 +17,15 @@ public class App
     {
         PlateWellHandler plateWellHandler = new PlateWellHandler();
 
+        System.out.println("Registering compound: S");
         plateWellHandler.registerCompound("S");
 
+        System.out.println("Adding plates: P-12345, P-3, P-7");
         plateWellHandler.addPlate("P-12345");
         plateWellHandler.addPlate("P-3");
         plateWellHandler.addPlate("P-7");
 
+        System.out.println("Adding compound S to Well P-12345.A1");
         plateWellHandler.addCompoundToWell("S", "P-12345.A1");
 
         ArrayList<String> newWells = new ArrayList<>();
@@ -30,7 +33,10 @@ public class App
         newWells.add("P-3.C2");
         newWells.add("P-7.A4");
 
+        System.out.println("Splitting contents from Well P-12345.A1 to Wells P-3.B4, P-3.C2, P-7.A4");
         plateWellHandler.splitCompoundToAdditionalWells("P-12345.A1", newWells);
-        System.out.println(plateWellHandler.getCompoundIdByWellId("P-7.A4"));
+
+        System.out.println("Retrieving contents of well P-7.A4");
+        System.out.println("Plate P-7.A4 contains compound: " + plateWellHandler.getCompoundIdByWellId("P-7.A4"));
     }
 }
