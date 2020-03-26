a4cf2767de567b19bdc2197d77d682cae6c251ca
ncassie <57377583+ncassie@users.noreply.github.com>
2020-03-25 21:46:07 -0400

    Update Well.java
    Initial implemenation of Well class.
    Contains a WellId and and a Compound.

--- kaleidochallenge/src/main/java/ncassie/domain/Well.java
+++ kaleidochallenge/src/main/java/ncassie/domain/Well.java
@@ -1,4 +1,38 @@
 package ncassie.domain;
 
+/**
+ * Well class - holds contents of well
+ */
 public class Well {
+
+    private String wellId;
+    private Compound wellCompound;
+
+    public Well(String wellId, Compound wellCompound) {
+        this.wellId = wellId;
+        this.wellCompound = wellCompound;
+    }
+
+    public Well(String wellId) {
+        this.wellId = wellId;
+    }
+
+    /**
+     *
+     * @param wellCompound - compound to add to well
+     * @throws Exception - assumption is a well with contents cannot receive a new compound, so throws an exception
+     * if well already contains compound
+     */
+    public void setWellCompound(Compound wellCompound) throws Exception {
+        if(this.wellCompound == null){
+            this.wellCompound = wellCompound;
+        } else{
+            throw new Exception("Well already full");
+        }
+
+    }
+
+    public Compound getWellCompound() {
+        return wellCompound;
+    }
 }
