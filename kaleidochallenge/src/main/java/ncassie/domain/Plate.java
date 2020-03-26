b33d3c2339bb2b7d9d74cd085e0143bde664361d
ncassie <57377583+ncassie@users.noreply.github.com>
2020-03-25 22:59:54 -0400

    Update Plate.java
    Fixed formatting of well ids

--- kaleidochallenge/src/main/java/ncassie/domain/Plate.java
+++ kaleidochallenge/src/main/java/ncassie/domain/Plate.java
@@ -11,20 +11,20 @@ public class Plate {
         plateWells = new Hashtable<String, Well>();
 
         // Assumption is that plates are standardized and come as described in problem set
-        plateWells.put(plateId + "A1", new Well(plateId + "A1"));
-        plateWells.put(plateId + "A2", new Well(plateId + "A2"));
-        plateWells.put(plateId + "A3", new Well(plateId + "A3"));
-        plateWells.put(plateId + "A4", new Well(plateId + "A4"));
+        plateWells.put(plateId + ".A1", new Well(plateId + ".A1"));
+        plateWells.put(plateId + ".A2", new Well(plateId + ".A2"));
+        plateWells.put(plateId + ".A3", new Well(plateId + ".A3"));
+        plateWells.put(plateId + ".A4", new Well(plateId + ".A4"));
 
-        plateWells.put(plateId + "B1", new Well(plateId + "B1"));
-        plateWells.put(plateId + "B2", new Well(plateId + "B2"));
-        plateWells.put(plateId + "B3", new Well(plateId + "B3"));
-        plateWells.put(plateId + "B4", new Well(plateId + "B4"));
+        plateWells.put(plateId + ".B1", new Well(plateId + ".B1"));
+        plateWells.put(plateId + ".B2", new Well(plateId + ".B2"));
+        plateWells.put(plateId + ".B3", new Well(plateId + ".B3"));
+        plateWells.put(plateId + ".B4", new Well(plateId + ".B4"));
 
-        plateWells.put(plateId + "C1", new Well(plateId + "C1"));
-        plateWells.put(plateId + "C2", new Well(plateId + "C2"));
-        plateWells.put(plateId + "C3", new Well(plateId + "C3"));
-        plateWells.put(plateId + "C4", new Well(plateId + "C4"));
+        plateWells.put(plateId + ".C1", new Well(plateId + ".C1"));
+        plateWells.put(plateId + ".C2", new Well(plateId + ".C2"));
+        plateWells.put(plateId + ".C3", new Well(plateId + ".C3"));
+        plateWells.put(plateId + ".C4", new Well(plateId + ".C4"));
 
     }
 
