24a3e933d8f47b5ac314bfac3a974ae77bbbd96a
ncassie <57377583+ncassie@users.noreply.github.com>
2020-03-25 21:55:30 -0400

    Update PlateWellHandler.java
    Add functionality of plates to handler.  Adds hashtable to store plates and method to allow plates to be added

--- kaleidochallenge/src/main/java/ncassie/handler/PlateWellHandler.java
+++ kaleidochallenge/src/main/java/ncassie/handler/PlateWellHandler.java
@@ -1,6 +1,7 @@
 package ncassie.handler;
 
 import ncassie.domain.Compound;
+import ncassie.domain.Plate;
 
 import java.util.Hashtable;
 
@@ -12,8 +13,12 @@ public class PlateWellHandler {
     // stores all compounds registered in program
     private Hashtable<String, Compound> compounds;
 
+    // stores all plates registered in program
+    private Hashtable<String, Plate> plates;
+
     public PlateWellHandler() {
         this.compounds = new Hashtable<String, Compound>();
+        this.plates = new Hashtable<String, Plate>();
     }
 
     /**
@@ -22,6 +27,14 @@ public class PlateWellHandler {
      */
     public void registerCompound(String compoundId){
         this.compounds.put(compoundId, new Compound(compoundId));
+    }
+
+    /**
+     * allows registration of plates
+     * @param plateId - id of plate to be registered
+     */
+    public void addPlate(String plateId){
+        plates.put(plateId, new Plate(plateId));
     }
 
 
