428a5dad3aa39e7715d0f0d31f16c5d7f2cbbe6b
ncassie <57377583+ncassie@users.noreply.github.com>
2020-03-25 21:52:17 -0400

    Update PlateWellHandler.java
    Initial implementation of PlateWellHandler
     - Adds hashable to store compounds, constructor and method to allow registration of compounds

--- kaleidochallenge/src/main/java/ncassie/handler/PlateWellHandler.java
+++ kaleidochallenge/src/main/java/ncassie/handler/PlateWellHandler.java
@@ -1,4 +1,28 @@
 package ncassie.handler;
 
+import ncassie.domain.Compound;
+
+import java.util.Hashtable;
+
+/**
+ * Handles functionality of Plate - Well system.
+ *
+ */
 public class PlateWellHandler {
+    // stores all compounds registered in program
+    private Hashtable<String, Compound> compounds;
+
+    public PlateWellHandler() {
+        this.compounds = new Hashtable<String, Compound>();
+    }
+
+    /**
+     * Allows registration of compounds
+     * @param compoundId - id of compound to be registered
+     */
+    public void registerCompound(String compoundId){
+        this.compounds.put(compoundId, new Compound(compoundId));
+    }
+
+
 }
